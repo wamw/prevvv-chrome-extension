2680822a99901efbec41621b22a6c8d91a6f9870
suttang <suttang@gmail.com>
2014-12-23 14:53:24 +0900

    画像コピータスク

---
+++ source/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+
+var conf = {
+	source: {
+		html: './html/**/!(_)*.html',
+		css: './scss/**/!(_)*.scss',
+		js: './js/**/*.js',
+		image: './images/**/*'
+	},
+	dest: {
+		html: '../build',
+		js: '../build/js',
+		css: '../build/css',
+		image: '../build/images'
+	}
+};
+
+gulp.task('build', function() {
+	
+});
+
+gulp.task('image', function() {
+	return gulp.src(conf.source.image)
+		.pipe(gulp.dest(conf.dest.image))
+		;
+});
